0e0fa8664c393c846c6e6a4fc02c86c779caa1a2
Cherryh4ck <tomas1234567891016@gmail.com>
2026-07-10 19:32:39 -0300

    rework nav

--- resources/nav.php
+++ resources/nav.php
@@ -1,3 +1,7 @@
+<?php
+    $nav_rol = $_SESSION["cuenta_rol"] ?? null;
+?>
+
 <nav>
     <p id="nav-logo">Tachibana</p>
     <ul>
@@ -9,13 +13,24 @@
     </ul>
     <div class="nav-cuenta">
         <?php if (!isset($_SESSION["cuenta_usuario"])): ?>
-            <a href="php/cuenta.php" id="cuenta">Anónimo</a>
-            <img src="resources/avatar.png" alt="">
+            <a href="php/cuenta.php" class="nav-cuenta-link">
+                <span class="nav-status-cuenta">
+                    <span class="nav-username">Anónimo</span>
+                </span>
+                <img src="resources/avatar.png" alt="" class="nav-avatar">
+            </a>
         <?php else:
             $avatar_propio = "resources/avatars/" . $_SESSION["cuenta_id"] . ".png";
         ?>
-            <a href="php/cuenta.php" id="cuenta"><?= e($_SESSION["cuenta_usuario"]) ?></a>
-            <?= avatar_img($avatar_propio) ?>
+            <a href="php/cuenta.php" class="nav-cuenta-link">
+                <span class="nav-status-cuenta">
+                    <span class="nav-username"><?= e($_SESSION["cuenta_usuario"]) ?></span>
+                    <?php if ($nav_rol): ?>
+                        <span id="input-tag-<?= e($nav_rol) ?>" class="nav-rol"><?= strtoupper(e($nav_rol)) ?></span>
+                    <?php endif; ?>
+                </span>
+                <?= avatar_img($avatar_propio, "class='nav-avatar'") ?>
+            </a>
         <?php endif; ?>
     </div>
 </nav>
