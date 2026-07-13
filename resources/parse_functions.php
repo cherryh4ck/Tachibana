f89b6a29c59ed558e36aba10d3c2422347b0e464
Cherryh4ck <tomas1234567891016@gmail.com>
2026-07-13 17:09:15 -0300

    calcular_tiempo() en parse_functions

--- resources/parse_functions.php
+++ resources/parse_functions.php
@@ -12,4 +12,25 @@
         }
         return "<img src='$src' alt='' $atributos_extra>";
     }
+
+    function calcular_tiempo(string $fecha): string {
+        $ahora = new DateTime();
+        $fecha_obj = new DateTime($fecha);
+        $diferencia = $ahora->diff($fecha_obj);
+
+        if ($diferencia->y > 0) {
+            return $diferencia->y . " año" . ($diferencia->y > 1 ? "s" : "");
+        } elseif ($diferencia->m > 0) {
+            return $diferencia->m . " mes" . ($diferencia->m > 1 ? "es" : "");
+        } elseif ($diferencia->d > 0) {
+            return $diferencia->d . " día" . ($diferencia->d > 1 ? "s" : "");
+        } elseif ($diferencia->h > 0) {
+            return $diferencia->h . " hora" . ($diferencia->h > 1 ? "s" : "");
+        } elseif ($diferencia->i > 0) {
+            return $diferencia->i . " minuto" . ($diferencia->i > 1 ? "s" : "");
+        } else {
+            $segundos = max(1, $diferencia->s);
+            return $segundos . " segundo" . ($segundos > 1 ? "s" : "");
+        }
+    }
 ?>
