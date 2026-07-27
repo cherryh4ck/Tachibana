163421c433ccf8519bdf551b502dfdcfa58ed779
Cherryh4ck <tomas1234567891016@gmail.com>
2026-07-27 19:10:18 -0300

    api/v1

---
+++ api/v1/posts/total.php
@@ -0,0 +1,24 @@
+<?php
+    require __DIR__ . '/../../../php/db/config.php';
+    header("Content-Type: application/json");
+
+    if ($conn_test == 0){
+        http_response_code(500);
+        echo json_encode(["ok" => false, "mensaje" => "Sin conexión."]);
+        exit();
+    }
+
+    try { 
+        $query = "SELECT COUNT(*) AS total FROM posts";
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        http_response_code(200);
+        echo json_encode(["ok" => true, "total" => $result['total']]);
+    }
+    catch (PDOException $e){
+        http_response_code(500);
+        echo json_encode(["ok" => false, "mensaje" => "Error al hacer la query."]);
+    }
+?>
