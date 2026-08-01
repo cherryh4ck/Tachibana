2e06d1dc4e3be6d8a162ff4c5e5fd508cea13d1e
Cherryh4ck <tomas1234567891016@gmail.com>
2026-08-01 04:21:45 -0300

    Incluir post ID como hidden value

--- resources/dialog-mod-ban.php
+++ resources/dialog-mod-ban.php
@@ -3,13 +3,14 @@
         <h2 class="contenido-setup-titulo">Banear post</h2>
         <script src="js/admin/modify-post.js" defer></script>
         <form action="php/setup.php" method="POST" id="formulario-setup">
+            <input type="hidden" name="postID" value="<?php echo $id; ?>">
             <p>Banear un post hará que este siga siendo visible, sin embargo los datos del post serán ocultados.</p>
             <br>
             <p>El motivo se mostrará en la descripción.</p>
             <div class="contenido-subir-formulario-fila1">
                 <div class="contenido-subir-formulario-fila1-input-allspace">
                     <p>Motivo del baneo</p>
-                    <input type="text" name="database" id="setup-database" placeholder="..." required>
+                    <input type="text" name="motivo" id="setup-motivo" placeholder="...">
                 </div>
             </div>
             <div class="contenido-subir-formulario-error">
