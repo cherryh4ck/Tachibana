d00505c38495d59edaa232a29bfb597a05c59c6d
Cherryh4ck <tomas1234567891016@gmail.com>
2026-08-09 02:44:45 -0300

    Remover código innecesario

--- reglas.php
+++ reglas.php
@@ -1,56 +1,5 @@
 <?php
     require "php/db/config.php";
-    if (isset($_SESSION["cuenta_usuario"])){
-        header("Location: index.php");
-        exit();
-    }
-
-    if (isset($_GET["reg"])){
-        $mensaje = "Usuario registrado, inicia sesión";
-    }
-
-    if ($_SERVER["REQUEST_METHOD"] === "POST"){
-        $username = trim($_POST["user"]);
-        $password = $_POST["password"];
-
-        if (empty($username) || empty($password)){
-            $mensaje = "<span>Error:</span> Los campos están vacios.";
-        }
-        else{
-            try{
-                $sql = $conn->prepare("SELECT * FROM usuarios WHERE username = ?");
-                $sql->execute([$username]);
-                $fetch = $sql->fetch(PDO::FETCH_ASSOC);
-
-                if ($fetch && password_verify($password, $fetch["password"])){
-                    // crear auth cookie
-                    $auth_cookie = bin2hex(random_bytes(128));
-                    $sql = $conn->prepare("UPDATE usuarios SET auth_cookie = ? WHERE id = ?");
-                    $sql->execute([$auth_cookie, $fetch["id"]]);
-
-                    $ult_act_activado = (int)$fetch["ult_act_activo"];
-
-                    setcookie("ult_act", $ult_act_activado, time() + (86400 * 30), "/"); 
-                    setcookie("auth", $auth_cookie, time() + (86400 * 30), "/"); 
-                    $_SESSION["cuenta_id"] = $fetch["id"];
-                    $_SESSION["cuenta_usuario"] = $fetch["username"];
-                    $_SESSION["cuenta_rol"] = $fetch["rol"];
-                    $_SESSION["error"] = 0;
-                    header("Location: index.php");
-                    exit();
-                }
-                else{
-                    $mensaje = "<span>Error:</span> Usuario o contraseña incorrecta";
-                }
-            }
-            catch(PDOException $e){
-                $mensaje = "<span>Error:</span> Ha ocurrido un error, intente más tarde";
-                if ($debug == 1) {
-                    echo $e;
-                }
-            }
-        }
-    }
 ?>
 
 <!DOCTYPE html>
