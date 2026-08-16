f94245b4180a4838ecd6c3fb0cef6c5614534c47
Cherryh4ck <tomas1234567891016@gmail.com>
2026-08-16 18:05:10 -0300

    Ventana modal de suspensión de usuarios

---
+++ resources/dialog-user-ban.php
@@ -0,0 +1,24 @@
+<dialog style="display: none;" class="contenido-subir contenido-setup" id="dialog-user-ban">
+    <div class="contenido-subir-formulario">
+        <h2 class="contenido-setup-titulo">Banear usuario</h2>
+        <script src="js/admin/modify-user.js" defer></script>
+        <form id="formulario-ban">
+            <div class="contenido-subir-formulario-fila1">
+                <div class="contenido-subir-formulario-fila1-input-allspace">
+                    <p>Motivo del baneo</p>
+                    <input type="text" name="motivo" id="ban-motivo" placeholder="...">
+                </div>
+            </div>
+            <div class="contenido-subir-formulario-fila1">
+                <div class="contenido-subir-formulario-fila1-input-allspace">
+                    <p>Baneado hasta</p>
+                    <input type="datetime-local" name="fecha_expiracion" id="ban-fecha-expiracion">
+                </div>
+            </div>
+            <div class="contenido-subir-formulario-error">
+                <p style="display: none;" id="mensaje-error"><span>Error al instalar:</span> Test test</p>
+            </div>
+            <input type="submit" value="Banear" id="setup-enviar">
+        </form>
+    </div>
+</dialog>
