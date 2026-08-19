4cd79027980c37bc7543962df88cbb845d41fec8
Cherryh4ck <tomas1234567891016@gmail.com>
2026-08-19 01:46:59 -0300

    Mejorar los comentarios

--- reglas.php
+++ reglas.php
@@ -3,9 +3,9 @@
     require "resources/parse_functions.php";
     /*
         IMPORTANTE:
-         Puedes modificar este archivo como se te antoje. 
-         No necesariamente estas reglas son definitivas.
-         Como tal, no deberías usar el contenido de las reglas de este archivo, ya que solo está lo más básico.
+          Puedes modificar este archivo como se te antoje. 
+          No necesariamente estas reglas son definitivas.
+          Como tal, no deberías usar el contenido de este archivo, es solo una idea.
     */
 ?>
 
